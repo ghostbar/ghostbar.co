b69c88f41b94564863e6a223d591b4ec0a662867
Jose Luis Rivas <me@ghostbar.co>
2014-05-29 01:41:49 -0500

    fixing the remote on buildcontrol

--- Gruntfile.js
+++ Gruntfile.js
@@ -288,7 +288,7 @@ module.exports = function (grunt) {
     buildcontrol: {
       dist: {
         options: {
-          remote: 'dist',
+          remote: '../',
           branch: 'gh-pages',
           commit: true,
           push: true
